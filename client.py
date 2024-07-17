75e22311e770dfcb5a7dfb2324ac3b1bc5973b14
Antonio <112651224+antoniodev0@users.noreply.github.com>
2024-07-17 15:05:41 +0200

    Update client.py

--- client.py
+++ client.py
@@ -26,48 +26,28 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-def load_datasets():
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+def load_datasets(partition_id):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
     train_dataset = datasets.MNIST(root='data', train=True, download=True, transform=transform)
     test_dataset = datasets.MNIST(root='data', train=False, download=True, transform=transform)
-    
+
     partition_size = len(train_dataset) // 5
-    partitions = random_split(train_dataset, [partition_size]*5)
-    
-    return partitions, test_dataset
+    partitions = random_split(train_dataset, [partition_size] * 5)
 
-def encrypt_parameters(parameters, context, batch_size=2048):
-    encrypted_parameters = []
-    for param in tqdm(parameters, desc="Encrypting Parameters"):
-        param_flat = param.flatten().tolist()
-        encrypted_batches = []
-        for i in range(0, len(param_flat), batch_size):
-            batch = param_flat[i:i+batch_size]
-            encrypted_tensor = ts.ckks_vector(context, batch)
-            encrypted_batches.append(encrypted_tensor.serialize())
-        encrypted_parameters.append((encrypted_batches, param.shape))
-    return encrypted_parameters
+    client_dataset = partitions[partition_id]
+    train_size = int(0.8 * len(client_dataset))
+    val_size = len(client_dataset) - train_size
+    train_dataset, val_dataset = random_split(client_dataset, [train_size, val_size])
 
-def decrypt_parameters(encrypted_parameters, context, batch_size=2048):
-    decrypted_parameters = []
-    for enc_batches, shape in tqdm(encrypted_parameters, desc="Decrypting Parameters"):
-        decrypted_flat = []
-        for enc_batch in enc_batches:
-            enc_tensor = ts.ckks_vector_from(context, enc_batch)
-            decrypted_flat.extend(enc_tensor.decrypt())
-        decrypted_tensor = torch.tensor(decrypted_flat).reshape(shape)
-        decrypted_parameters.append(decrypted_tensor)
-    return decrypted_parameters
-
-def serialize_parameters(encrypted_parameters):
-    return pickle.dumps(encrypted_parameters)
-
-def deserialize_parameters(serialized_parameters):
-    return pickle.loads(serialized_parameters)
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
+    return train_loader, val_loader, test_dataset
 
 def client_fn(partition_id):
-    partitions, _ = load_datasets()
-    train_loader = DataLoader(partitions[partition_id], batch_size=16, shuffle=True)  # Reduced batch size
+    train_loader, val_loader, test_dataset = load_datasets(partition_id)
 
     model = CNN()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
@@ -75,6 +55,35 @@ def client_fn(partition_id):
     context = ts.context(ts.SCHEME_TYPE.CKKS, poly_modulus_degree=8192, coeff_mod_bit_sizes=[60, 40, 40, 60])
     context.global_scale = 2**40
     context.generate_galois_keys()
+
+    def encrypt_parameters(parameters, context, batch_size=256):  # Batch size modificabile
+        encrypted_parameters = []
+        for param in tqdm(parameters, desc="Encrypting Parameters"):
+            param_flat = param.flatten().tolist()
+            encrypted_batches = []
+            for i in range(0, len(param_flat), batch_size):
+                batch = param_flat[i:i+batch_size]
+                encrypted_tensor = ts.ckks_vector(context, batch)
+                encrypted_batches.append(encrypted_tensor.serialize())
+            encrypted_parameters.append((encrypted_batches, param.shape))
+        return encrypted_parameters
+
+    def decrypt_parameters(encrypted_parameters, context, batch_size=256):  # Batch size modificabile
+        decrypted_parameters = []
+        for enc_batches, shape in tqdm(encrypted_parameters, desc="Decrypting Parameters"):
+            decrypted_flat = []
+            for enc_batch in enc_batches:
+                enc_tensor = ts.ckks_vector_from(context, enc_batch)
+                decrypted_flat.extend(enc_tensor.decrypt())
+            decrypted_tensor = torch.tensor(decrypted_flat).reshape(shape)
+            decrypted_parameters.append(decrypted_tensor)
+        return decrypted_parameters
+
+    def serialize_parameters(encrypted_parameters):
+        return pickle.dumps(encrypted_parameters)
+
+    def deserialize_parameters(serialized_parameters):
+        return pickle.loads(serialized_parameters)
 
     class FlowerClient(fl.client.NumPyClient):
         def get_parameters(self, config=None):
@@ -94,28 +103,31 @@ def client_fn(partition_id):
         def fit(self, parameters, config):
             self.set_parameters(parameters)
             model.train()
+            total_loss = 0
             for epoch in tqdm(range(1), desc="Training Epochs"):
                 for batch_idx, (data, target) in tqdm(enumerate(train_loader), desc="Training Batches", total=len(train_loader)):
                     optimizer.zero_grad()
                     output = model(data)
                     loss = F.nll_loss(output, target)
+                    total_loss += loss.item() * len(data)
                     loss.backward()
                     optimizer.step()
-            return self.get_parameters(), len(train_loader.dataset), {}
+            average_loss = total_loss / len(train_loader.dataset)
+            return self.get_parameters(), len(train_loader.dataset), {"loss": average_loss}
 
         def evaluate(self, parameters, config):
             self.set_parameters(parameters)
-            _, test_dataset = load_datasets()
-            test_loader = DataLoader(test_dataset, batch_size=16)
             model.eval()
-            correct = 0
+            test_loader = DataLoader(test_dataset, batch_size=16)
+            total_loss = 0
             with torch.no_grad():
                 for data, target in tqdm(test_loader, desc="Evaluating Batches"):
-                    output = model(data)
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(target.view_as(pred)).sum().item()
-            accuracy = correct / len(test_loader.dataset)
-            return float(accuracy), len(test_loader.dataset), {"accuracy": accuracy}
+                    encrypted_data = ts.ckks_vector(context, data.flatten().tolist())
+                    encrypted_output = model(encrypted_data)
+                    decrypted_output = encrypted_output.decrypt()
+                    loss = F.nll_loss(torch.tensor(decrypted_output), target)
+                    total_loss += loss.item()
+            return float(total_loss) / len(test_loader), len(test_loader.dataset), {"loss": float(total_loss) / len(test_loader.dataset)}
 
     return FlowerClient()
 
